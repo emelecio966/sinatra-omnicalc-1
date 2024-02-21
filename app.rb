afd4faecf2aa2394901f38e0a86e91e01f93c976
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-02-21 15:37:57 +0000

    views

--- app.rb
+++ app.rb
@@ -5,6 +5,14 @@ get("/howdy") do
   erb(:hello)
 end 
 
+get("/goodbye") do
+   erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
