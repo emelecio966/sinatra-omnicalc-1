d4cb42a6285c97f6cd66d28d7f12606a7883e6e0
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-03-04 15:38:43 +0000

    random calc

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
 get("/howdy") do
   erb(:hello)
 end 
@@ -9,8 +16,20 @@ get("/goodbye") do
    erb(:bye)
 end
 
-get("/square/new") do
-  erb(:new_square_calc)
+get("/square/new")do
+   @the_num = params.fetch("users_number").to_f
+
+   @the_result = @the_num **2  
+
+   erb(:new_square_calc)
+end
+
+get("/square_new/results") do
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
 end
 
 get ("/square_root/results") do
@@ -19,6 +38,10 @@ get ("/square_root/results") do
   @the_result = @the_num **2
 
   erb(:square_results)
+end
+
+get("/payment/new") do
+  erb(:new_payment_calc)
 end
 
 get ("/payment/results") do
@@ -30,10 +53,10 @@ get ("/payment/results") do
   erb(:payment_results)
 end
 
+get("/random/new") do
+  erb(:new_random_calc)
+end
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/random/results") do
+  erb(:random_results)
 end
